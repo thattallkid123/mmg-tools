11c3c93d4d7948525c0185b1c55154c39db85adf
Andy Griffiths <andy@mrmallorcagolf.com>
2026-05-04 00:31:49 +0200

    Add strokes gained static app

---
+++ strokes-gained/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'mmg-player-strokes-gained-v6';
+const ASSETS = ['./index.html', './manifest.webmanifest', './icon.svg', './favicon.svg'];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
